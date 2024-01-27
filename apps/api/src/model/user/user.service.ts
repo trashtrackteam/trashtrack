30fc2f677db2c2e43295808748994cb220ea6955
NotHydra <rizky.irswanda115@gmail.com>
2024-01-28 00:00:32 +0800

    feat: user service

---
+++ apps/api/src/model/user/user.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "../../provider/prisma.service";
+import { UserModel } from "./user.model";
+import { UserDTOInterface } from "./user.interface";
+
+@Injectable()
+export class UserService {
+    constructor(private readonly prisma: PrismaService) {}
+
+    async find(): Promise<UserModel[]> {
+        return await this.prisma.user.findMany();
+    }
+
+    async findId(id: number): Promise<UserModel> {
+        return await this.prisma.user.findUnique({
+            where: { id },
+        });
+    }
+
+    async add(payload: UserDTOInterface): Promise<UserModel> {
+        return await this.prisma.user.create({ data: payload });
+    }
+
+    async change(id: number, payload: UserDTOInterface): Promise<UserModel> {
+        return await this.prisma.user.update({
+            where: { id },
+            data: payload,
+        });
+    }
+
+    async remove(id: number): Promise<UserModel> {
+        return await this.prisma.user.delete({
+            where: { id },
+        });
+    }
+}
