b1ab36be095c763fd3c9de0ecde02c0f4af5a0b0
NotHydra <rizky.irswanda115@gmail.com>
2024-01-27 19:14:27 +0800

    refactor: port changes

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,18 +3,20 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
+import { INestApplication, Logger } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
 
-import { AppModule } from './app/app.module';
+import { AppModule } from "./app/app.module";
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
+async function bootstrap(): Promise<void> {
+    const app: INestApplication<AppModule> = await NestFactory.create<INestApplication<AppModule>>(AppModule);
+    const port: number = parseInt(process.env.PORT) || 3001;
+    const globalPrefix: string = "api";
+
+    app.setGlobalPrefix(globalPrefix);
+    await app.listen(port);
+
+    Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
 }
 
 bootstrap();
