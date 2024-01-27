1b4f55ac78892ff22dae10cd2f9e72d1d850fb3e
NotHydra <rizky.irswanda115@gmail.com>
2024-01-27 19:51:28 +0800

    refactor: remove controller and service

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,11 +1,8 @@
-import { Module } from '@nestjs/common';
-
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [],
+    controllers: [],
+    providers: [],
 })
 export class AppModule {}
