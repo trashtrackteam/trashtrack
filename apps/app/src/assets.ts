b0335c54699e6121159f0c02ff16011e84615866
elizielx <yehezkieldio35@gmail.com>
2024-01-28 20:39:30 +0800

    feat: add carousel on onboarding

--- apps/app/src/assets.ts
+++ apps/app/src/assets.ts
@@ -1,5 +1,7 @@
 const IMAGES = {
-    onboarding_icon: new URL("./assets/onboarding.svg", import.meta.url).href,
+    onboarding_icon_one: new URL("./assets/onboarding_one.svg", import.meta.url).href,
+    onboarding_icon_two: new URL("./assets/onboarding_two.svg", import.meta.url).href,
+    onboarding_icon_three: new URL("./assets/onboarding_three.svg", import.meta.url).href,
 };
 
 export default IMAGES;
--- apps/app/src/pages/onboarding.tsx
+++ apps/app/src/pages/onboarding.tsx
@@ -1,5 +1,5 @@
 import { IonPage, IonContent, useIonRouter } from "@ionic/react";
-import { Button } from "@trashtrack/ui";
+import { Button, Carousel, CarouselContent, CarouselItem } from "@trashtrack/ui";
 import IMAGES from "../assets";
 
 export function Onboarding() {
@@ -11,16 +11,62 @@ export function Onboarding() {
                 <div className="pt-12 pb-12">
                     <h1 className="font-bold text-center text-xl">TrashTrack</h1>
                 </div>
-                <div className="flex flex-col justify-center items-center">
-                    <img className="w-[154px] h-[152px]" src={IMAGES.onboarding_icon} alt="Onboarding Icon" />
-                    <div className="text-center pt-7 pb-32 px-11">
-                        <h2 className="pb-3 font-semibold text-xl">Solusi Inovatif</h2>
-                        <p className="text-slate-600 text-[10px]">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus ducimus in odio quasi
-                            tempora? Dolorum nobis molestias ut.
-                        </p>
-                    </div>
-                </div>
+                <Carousel
+                    opts={{
+                        loop: true,
+                    }}
+                >
+                    <CarouselContent>
+                        <CarouselItem>
+                            <div className="flex flex-col justify-center items-center">
+                                <img
+                                    className="w-[154px] h-[152px]"
+                                    src={IMAGES.onboarding_icon_one}
+                                    alt="Onboarding Icon"
+                                />
+                                <div className="text-center pt-7 pb-40 px-11">
+                                    <h2 className="pb-3 font-semibold text-xl">Waste Analytics</h2>
+                                    <p className="text-slate-600 text-[10px]">
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus ducimus in
+                                        odio quasi tempora? Dolorum nobis molestias ut.
+                                    </p>
+                                </div>
+                            </div>
+                        </CarouselItem>
+                        <CarouselItem>
+                            <div className="flex flex-col justify-center items-center">
+                                <img
+                                    className="w-[154px] h-[152px]"
+                                    src={IMAGES.onboarding_icon_two}
+                                    alt="Onboarding Icon"
+                                />
+                                <div className="text-center pt-7 pb-40 px-11">
+                                    <h2 className="pb-3 font-semibold text-xl">Waste Management</h2>
+                                    <p className="text-slate-600 text-[10px]">
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus ducimus in
+                                        odio quasi tempora? Dolorum nobis molestias ut.
+                                    </p>
+                                </div>
+                            </div>
+                        </CarouselItem>
+                        <CarouselItem>
+                            <div className="flex flex-col justify-center items-center">
+                                <img
+                                    className="w-[154px] h-[152px]"
+                                    src={IMAGES.onboarding_icon_three}
+                                    alt="Onboarding Icon"
+                                />
+                                <div className="text-center pt-7 pb-40 px-11">
+                                    <h2 className="pb-3 font-semibold text-xl">Waste Monitoring</h2>
+                                    <p className="text-slate-600 text-[10px]">
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus ducimus in
+                                        odio quasi tempora? Dolorum nobis molestias ut.
+                                    </p>
+                                </div>
+                            </div>
+                        </CarouselItem>
+                    </CarouselContent>
+                </Carousel>
                 <div className="flex flex-col justify-center items-center px-12">
                     <Button
                         className="w-full h-[46px] font-bold text-xs"
