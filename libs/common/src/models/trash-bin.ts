45750f81a3491e58f1829a322d33f75d436e56e8
NotHydra <rizky.irswanda115@gmail.com>
2024-02-06 23:22:45 +0800

    feat: trash bin model

---
+++ libs/common/src/models/trash-bin.ts
@@ -0,0 +1,33 @@
+import { Prisma } from "@prisma/client";
+import { IsDate, IsNumber, IsOptional, IsString } from "class-validator";
+
+/**
+ * Represents a trash bin in the system.
+ */
+export class TrashBinModel implements Prisma.TrashBinCreateInput {
+    @IsNumber()
+    id: number
+
+    @IsString()
+    name: string;
+
+    @IsNumber()
+    latitude: number;
+
+    @IsNumber()
+    longitude: number;
+
+    @IsOptional()
+    @IsString()
+    description?: string;
+
+    @IsNumber()
+    openCount: number;
+
+    @IsDate()
+    createdAt: Date;
+
+    @IsDate()
+    updatedAt: Date;
+    
+}
