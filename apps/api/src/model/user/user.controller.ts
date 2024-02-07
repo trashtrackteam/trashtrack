766be166dc72ff2d2f6269911e82ef965e9d3a44
NotHydra <rizky.irswanda115@gmail.com>
2024-02-07 19:02:43 +0800

    feat: user controller extend base controller

--- apps/api/src/model/user/user.controller.ts
+++ apps/api/src/model/user/user.controller.ts
@@ -1,99 +1,35 @@
 import {
-    Get,
-    Post,
-    Put,
-    Delete,
-    Body,
-    Param,
-    Controller,
-    ParseIntPipe,
-    UseInterceptors,
-    NotFoundException,
     BadRequestException,
+    Body,
+    Controller,
+    Get,
+    NotFoundException,
+    Param,
+    ParseIntPipe,
+    Put,
 } from "@nestjs/common";
-import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 import {
     UserModel,
     UserCreateDTO,
-    UserUpdateActiveDTO,
     UserUpdateDTO,
-    UserUpdatePasswordDTO,
     ResponseFormatInterface,
+    UserUpdateActiveDTO,
+    UserUpdatePasswordDTO,
 } from "@trashtrack/common";
 
-import { ResponseFormatInterceptor, formatResponse } from "../../interceptor/response-format.interceptor";
 
-import { LoggerService } from "../../provider/logger.service";
+import { BaseController } from "../base.controller";
 
 import { UserService } from "./user.service";
+import { formatResponse } from "../../interceptor/response-format.interceptor";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 
-/**
- * Controller for handling user-related operations.
- */
 @Controller("user")
-@UseInterceptors(ResponseFormatInterceptor)
-export class UserController {
-    private readonly loggerService: LoggerService = new LoggerService(UserController.name);
-
-    constructor(private readonly userService: UserService) {}
-
-    /**
-     * Get all users.
-     * @returns A promise that resolves to an array of UserModel.
-     */
-    @Get()
-    public async find(): Promise<ResponseFormatInterface<UserModel[]>> {
-        try {
-            const response: ResponseFormatInterface<UserModel[]> = formatResponse<UserModel[]>(
-                true,
-                200,
-                "Found",
-                await this.userService.find()
-            );
-
-            this.loggerService.log(`Find: ${JSON.stringify(response)}`);
-
-            return response;
-        } catch (error) {
-            this.loggerService.error(`Find: ${error.message}`);
-            return formatResponse<null>(false, 500, error.message, null);
-        }
+export class UserController extends BaseController<UserModel, UserCreateDTO, UserUpdateDTO, UserService>{
+    constructor(modelService: UserService) {
+        super(UserController.name, modelService);
     }
 
-    /**
-     * Get a user by Id.
-     * @param id - The Id of the user to be found.
-     * @returns A promise that resolves to a UserModel.
-     */
-    @Get("id/:id")
-    public async findId(@Param("id", ParseIntPipe) id: number) {
-        try {
-            const response: ResponseFormatInterface<UserModel> = formatResponse<UserModel>(
-                true,
-                200,
-                "Id Found",
-                await this.userService.findId(id)
-            );
-
-            this.loggerService.log(`Find Id: ${JSON.stringify(response)}`);
-
-            return response;
-        } catch (error) {
-            if (error instanceof NotFoundException) {
-                this.loggerService.error(`Find Id: ${error.message}`);
-                return formatResponse<null>(false, 404, error.message, null);
-            }
-
-            this.loggerService.error(`Find Id: ${error.message}`);
-            return formatResponse<null>(false, 500, error.message, null);
-        }
-    }
-
-    /**
-     * Get a user by username.
-     * @param username - The username of the user to be found.
-     * @returns A promise that resolves to a UserModel.
-     */
     @Get("username/:username")
     public async findUsername(@Param("username") username: string): Promise<ResponseFormatInterface<UserModel>> {
         try {
@@ -101,7 +37,7 @@ export class UserController {
                 true,
                 200,
                 "Username Found",
-                await this.userService.findUsername(username)
+                await this.modelService.findUsername(username)
             );
 
             this.loggerService.log(`Find Username: ${JSON.stringify(response)}`);
@@ -118,79 +54,6 @@ export class UserController {
         }
     }
 
-    /**
-     * Add a new user.
-     * @param payload - The user data to be added.
-     * @returns A promise that resolves to a UserModel.
-     */
-    @Post()
-    public async add(@Body() payload: UserCreateDTO): Promise<ResponseFormatInterface<UserModel>> {
-        try {
-            const response: ResponseFormatInterface<UserModel> = formatResponse<UserModel>(
-                true,
-                201,
-                "Added",
-                await this.userService.add(payload)
-            );
-
-            this.loggerService.log(`Add: ${JSON.stringify(response)}`);
-
-            return response;
-        } catch (error) {
-            if (error instanceof BadRequestException) {
-                this.loggerService.error(`Add: ${error.message}`);
-                return formatResponse<null>(false, 400, error.message, null);
-            }
-
-            this.loggerService.error(`Add: ${error.message}`);
-            return formatResponse<null>(false, 500, error.message, null);
-        }
-    }
-
-    /**
-     * Update a user by ID.
-     * @param id - The ID of the user to be updated.
-     * @param payload - The updated user data.
-     * @returns A promise that resolves to a UserModel.
-     */
-    @Put(":id")
-    public async change(
-        @Param("id", ParseIntPipe) id: number,
-        @Body() payload: UserUpdateDTO
-    ): Promise<ResponseFormatInterface<UserModel>> {
-        try {
-            const response: ResponseFormatInterface<UserModel> = formatResponse<UserModel>(
-                true,
-                200,
-                "Changed",
-                await this.userService.change(id, payload)
-            );
-
-            this.loggerService.log(`Change: ${JSON.stringify(response)}`);
-
-            return response;
-        } catch (error) {
-            if (error instanceof BadRequestException) {
-                this.loggerService.error(`Change: ${error.message}`);
-                return formatResponse<null>(false, 400, error.message, null);
-            }
-
-            if (error instanceof NotFoundException || error instanceof PrismaClientKnownRequestError) {
-                this.loggerService.error(`Change: ${error.message}`);
-                return formatResponse<null>(false, 404, error.message, null);
-            }
-
-            this.loggerService.error(`Change: ${error.message}`);
-            return formatResponse<null>(false, 500, error.message, null);
-        }
-    }
-
-    /**
-     * Update a user's password by ID.
-     * @param id - The ID of the user to be updated.
-     * @param payload - The updated user password data.
-     * @returns A promise that resolves to a UserModel.
-     */
     @Put(":id/password")
     public async changePassword(
         @Param("id", ParseIntPipe) id: number,
@@ -201,7 +64,7 @@ export class UserController {
                 true,
                 200,
                 "Password Changed",
-                await this.userService.changePassword(id, payload)
+                await this.modelService.changePassword(id, payload)
             );
 
             this.loggerService.log(`Change Password: ${JSON.stringify(response)}`);
@@ -223,12 +86,6 @@ export class UserController {
         }
     }
 
-    /**
-     * Update a user's active status by ID.
-     * @param id - The ID of the user to be updated.
-     * @param payload - The updated user active status data.
-     * @returns A promise that resolves to a UserModel.
-     */
     @Put(":id/active")
     public async changeActive(@Param("id", ParseIntPipe) id: number, @Body() payload: UserUpdateActiveDTO) {
         try {
@@ -236,7 +93,7 @@ export class UserController {
                 true,
                 200,
                 "Active Changed",
-                await this.userService.changeActive(id, payload)
+                await this.modelService.changeActive(id, payload)
             );
 
             this.loggerService.log(`Change Active: ${JSON.stringify(response)}`);
@@ -249,35 +106,6 @@ export class UserController {
             }
 
             this.loggerService.error(`Change Active: ${error.message}`);
-            return formatResponse<null>(false, 500, error.message, null);
-        }
-    }
-
-    /**
-     * Delete a user by ID.
-     * @param id - The ID of the user to be deleted.
-     * @returns A promise that resolves to a UserModel.
-     */
-    @Delete(":id")
-    public async remove(@Param("id", ParseIntPipe) id: number): Promise<ResponseFormatInterface<UserModel>> {
-        try {
-            const response: ResponseFormatInterface<UserModel> = formatResponse<UserModel>(
-                true,
-                200,
-                "Removed",
-                await this.userService.remove(id)
-            );
-
-            this.loggerService.log(`Remove: ${JSON.stringify(response)}`);
-
-            return response;
-        } catch (error) {
-            if (error instanceof NotFoundException || error instanceof PrismaClientKnownRequestError) {
-                this.loggerService.error(`Remove: ${error.message}`);
-                return formatResponse<null>(false, 404, error.message, null);
-            }
-
-            this.loggerService.error(`Remove: ${error.message}`);
             return formatResponse<null>(false, 500, error.message, null);
         }
     }
