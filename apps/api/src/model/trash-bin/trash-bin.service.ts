899b4b70ca3a41e68e5a971d6a194e42da8fb4d3
NotHydra <rizky.irswanda115@gmail.com>
2024-02-08 20:56:32 +0800

    feat: trash bin open count

--- apps/api/src/model/trash-bin/trash-bin.service.ts
+++ apps/api/src/model/trash-bin/trash-bin.service.ts
@@ -1,11 +1,14 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, InternalServerErrorException, NotFoundException } from "@nestjs/common";
 import { TrashBinCreateDTO, TrashBinModel, TrashBinUpdateDTO } from "@trashtrack/common";
 
 import { ExtendService } from "../extend.service";
 
 import { PrismaService } from "../../provider/prisma.service";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 
-interface TrashBinServiceInterface {}
+interface TrashBinServiceInterface {
+    increaseOpenCount(id: number): Promise<TrashBinModel>;
+}
 
 @Injectable()
 export class TrashBinService
@@ -14,5 +17,44 @@ export class TrashBinService
 {
     constructor(prismaService: PrismaService) {
         super(TrashBinService.name, prismaService, { subTrashBin: true });
+    }
+
+    public async increaseOpenCount(id: number): Promise<TrashBinModel> {
+        try {
+            const dataModel: { openCount: number } = await this.prismaService[this.modelName].findUnique({
+                where: { id: id },
+                select: { openCount: true },
+            });
+
+            if (!dataModel) {
+                throw new NotFoundException(`Id ${id} Not Found`);
+            }
+
+            const model: TrashBinModel = await this.prismaService[this.modelName].update({
+                where: { id },
+                data: { openCount: dataModel.openCount + 1 },
+            });
+
+            if (!model) {
+                throw new NotFoundException(`Id ${id} Not Found`);
+            }
+
+            this.loggerService.log(`Increase Open Count: ${JSON.stringify(model)}`);
+
+            return model;
+        } catch (error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+                this.loggerService.error(`Increase Open Count: Id ${id} Not Found`);
+                throw new NotFoundException(`Id ${id} Not Found`);
+            }
+
+            if (error instanceof NotFoundException) {
+                this.loggerService.error(`Increase Open Count: ${error.message}`);
+                throw error;
+            }
+
+            this.loggerService.error(`Increase Open Count: ${error.message}`);
+            throw new InternalServerErrorException("Internal Server Error");
+        }
     }
 }
