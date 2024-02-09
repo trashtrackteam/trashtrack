23c9cb6ffa803666e83c9495f1f1645484628b9a
NotHydra <rizky.irswanda115@gmail.com>
2024-02-09 13:58:31 +0800

    feat: feedback service #17

---
+++ apps/api/src/model/feedback/feedback.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from "@nestjs/common";
+import { FeedbackCreateDTO, FeedbackModel, FeedbackUpdateDTO } from "@trashtrack/common";
+
+import { BaseService } from "../base.service";
+
+import { PrismaService } from "../../provider/prisma.service";
+
+interface FeedbackInterface {}
+
+@Injectable()
+export class FeedbackService
+    extends BaseService<FeedbackModel, FeedbackCreateDTO, FeedbackUpdateDTO>
+    implements FeedbackInterface
+{
+    constructor(prismaService: PrismaService) {
+        super(FeedbackService.name, prismaService);
+    }
+}
