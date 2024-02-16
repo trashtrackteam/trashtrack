b1093bd6f1ffd232765e9ea3f6d3d9138caf5518
elizielx <yehezkieldio35@gmail.com>
2024-02-16 11:02:16 +0800

    refactor: use prod

--- libs/utils/src/app-constants.ts
+++ libs/utils/src/app-constants.ts
@@ -1,4 +1,5 @@
 /**
  * !IMPORTANT - Change this on production.
  */
-export const API_URL = "https://ac2c-36-85-7-42.ngrok-free.app/api";
+// export const API_URL = "https://ac2c-36-85-7-42.ngrok-free.app/api";
+export const API_URL = "https://trashtrack-api.irswanda.com/api";
