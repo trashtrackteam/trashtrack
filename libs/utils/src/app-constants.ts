0f162b373882653c2e73b2358dd935976a75a012
elizielx <yehezkieldio35@gmail.com>
2024-02-18 11:08:13 +0800

    refactor: switch to production api

--- libs/utils/src/app-constants.ts
+++ libs/utils/src/app-constants.ts
@@ -1,5 +1,5 @@
 /**
  * !IMPORTANT - Change this on production.
  */
-export const API_URL = "https://7xfwrx7k-3030.asse.devtunnels.ms/api";
-// export const API_URL = "https://trashtrack-api.irswanda.com/api";
+// export const API_URL = "https://7xfwrx7k-3030.asse.devtunnels.ms/api";
+export const API_URL = "https://trashtrack-api.irswanda.com/api";
