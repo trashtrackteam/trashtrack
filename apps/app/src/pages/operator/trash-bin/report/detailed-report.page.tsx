ec9abd0099f65f29a3866179cbc54c0896033051
elizielx <yehezkieldio35@gmail.com>
2024-02-18 11:06:40 +0800

    feat: wip translations

--- apps/app/src/pages/operator/trash-bin/report/detailed-report.page.tsx
+++ apps/app/src/pages/operator/trash-bin/report/detailed-report.page.tsx
@@ -125,6 +125,7 @@ export function DetailedReportPage() {
             <IonContent className="ion-padding" fullscreen>
                 <div className="pt-12">
                     <h1 className="font-bold text-left text-xl">TrashTrack</h1>
+                    <p className="text-xs text-left text-slate-600">Detailed view of the report.</p>
                 </div>
                 <div className="flex flex-col pt-8 gap-2">
                     {isError && (
