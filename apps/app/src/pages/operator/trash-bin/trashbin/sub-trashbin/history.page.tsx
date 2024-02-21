442afe0c4ed38e71e4a6ae6a6e8b914c3a6035d4
elizielx <yehezkieldio35@gmail.com>
2024-02-21 11:17:07 +0800

    feat(app): translations fix 2

--- apps/app/src/pages/operator/trash-bin/trashbin/sub-trashbin/history.page.tsx
+++ apps/app/src/pages/operator/trash-bin/trashbin/sub-trashbin/history.page.tsx
@@ -78,7 +78,7 @@ export function TrashPage() {
                 </IonRefresher>
                 <div className="pt-12">
                     <h1 className="font-bold text-left text-xl">TrashTrack</h1>
-                    <p className="text-xs text-left text-slate-600">History</p>
+                    <p className="text-xs text-left text-slate-600">{t("operator.subtrashbin.history.subtitle")}</p>
                 </div>
                 <div className="flex flex-col pt-8 gap-2">
                     <div className="flex flex-col gap-2">
