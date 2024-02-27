ce4b1711c0038b0a036dc0d6d524d8f2b0ced287
elizielx <yehezkieldio35@gmail.com>
2024-02-27 19:42:14 +0800

    feat(bot): skeleton files and routes for user

--- apps/app/src/components/operator-tabs.tsx
+++ apps/app/src/components/operator-tabs.tsx
@@ -5,6 +5,10 @@ import { Icons, OperatorContext } from "@trashtrack/ui";
 
 import OperatorDashboard from "../pages/operator/dashboard";
 import { useContext } from "react";
+import { UsersPage } from "../pages/operator/user/users.page";
+import { DetailedUserPage } from "../pages/operator/user/detailed-user.page";
+import { ChangeUserPage } from "../pages/operator/user/change-user.page";
+import { CreateUserPage } from "../pages/operator/user/create-user.page";
 
 const OperatorTabs: React.FC = () => {
     const operator = useContext(OperatorContext);
@@ -15,6 +19,11 @@ const OperatorTabs: React.FC = () => {
         <IonTabs>
             <IonRouterOutlet animated={false} mode="ios">
                 <Route path="/operator/tabs/dashboard" render={() => <OperatorDashboard />} exact={true} />
+
+                <Route path="/operator/tabs/user" render={() => <UsersPage />} exact={true} />
+                <Route path="/operator/tabs/user/create" render={() => <CreateUserPage />} exact={true} />
+                <Route path="/operator/tabs/user/details/:user-id" render={() => <DetailedUserPage />} exact={true} />
+                <Route path="/operator/tabs/user/update/:user-id" render={() => <ChangeUserPage />} exact={true} />
 
                 <Route path="/operator/tabs" render={() => <Redirect to="/operator/tabs/dashboard" />} exact={true} />
             </IonRouterOutlet>
@@ -27,12 +36,12 @@ const OperatorTabs: React.FC = () => {
                     <Icons.trash strokeWidth={1} className="pt-2 w-[32px] h-[30px]" />
                     <IonLabel className="pt-2 pb-2">{t("tabs.trashbin")}</IonLabel>
                 </IonTabButton>
-                {/* {isAdmin && (
+                {isAdmin && (
                     <IonTabButton tab="userOperatorTab" href="/operator/tabs/user">
                         <Icons.user strokeWidth={1} className="pt-2 w-[32px] h-[30px]" />
                         <IonLabel className="pt-2 pb-2">User</IonLabel>
                     </IonTabButton>
-                )} */}
+                )}
             </IonTabBar>
         </IonTabs>
     );
---
+++ apps/app/src/pages/operator/user/detailed-user.page.tsx
@@ -0,0 +1,7 @@
+export function DetailedUserPage() {
+    return (
+        <div>
+            <div></div>
+        </div>
+    );
+}
