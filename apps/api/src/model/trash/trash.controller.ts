312c4e9c276fb31e17a4609f3861f0b358fb4fcf
NotHydra <rizky.irswanda115@gmail.com>
2024-03-02 11:21:49 +0800

    feat: trash area chart total endpoint

--- apps/api/src/model/trash/trash.controller.ts
+++ apps/api/src/model/trash/trash.controller.ts
@@ -1,7 +1,7 @@
-import { Body, Controller, ForbiddenException, Param, ParseIntPipe, UseInterceptors } from "@nestjs/common";
+import { Body, Controller, ForbiddenException, Get, Param, ParseIntPipe, UseInterceptors } from "@nestjs/common";
 import { Override, ResponseFormatInterface, TrashCreateDTO, TrashModel, TrashUpdateDTO } from "@trashtrack/common";
 
-import { ResponseFormatInterceptor } from "../../interceptor/response-format.interceptor";
+import { ResponseFormatInterceptor, formatResponse } from "../../interceptor/response-format.interceptor";
 
 import { BaseController } from "../../global/base.controller";
 
@@ -28,5 +28,24 @@ export class TrashController
     ): Promise<ResponseFormatInterface<TrashModel>> {
         this.loggerService.error(`Change: Method Is Disabled`);
         throw new ForbiddenException("Method Is Disabled");
+    }
+
+    @Get("area-chart-total")
+    public async findAreaChartTotal(): Promise<ResponseFormatInterface<{ name: string; total: number }[]>> {
+        try {
+            const response: ResponseFormatInterface<{ name: string; total: number }[]> = formatResponse<
+                {
+                    name: string;
+                    total: number;
+                }[]
+            >(true, 200, "Area Chart Total Found", await this.modelService.findAreaChartTotal());
+
+            this.loggerService.log(`Find Area Chart Total: ${JSON.stringify(response)}`);
+
+            return response;
+        } catch (error) {
+            this.loggerService.error(`Find Area Chart Total: ${error.message}`);
+            return formatResponse<null>(false, 500, error.message, null);
+        }
     }
 }
--- apps/api/src/model/trash/trash.service.ts
+++ apps/api/src/model/trash/trash.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, InternalServerErrorException } from "@nestjs/common";
 import { TrashCreateDTO, TrashModel, TrashUpdateDTO } from "@trashtrack/common";
 
 import { BaseService } from "../../global/base.service";
@@ -14,5 +14,39 @@ export class TrashService
 {
     constructor(prismaService: PrismaService) {
         super(TrashService.name, prismaService);
+    }
+
+    public async findAreaChartTotal(): Promise<{ name: string; total: number }[]> {
+        try {
+            const today: Date = new Date();
+            const data: { name: string; total: number }[] = await Promise.all(
+                [0, 1, 2, 3, 4, 5, 6].map(async (value: number): Promise<{ name: string; total: number }> => {
+                    const ltDate: Date = new Date(today);
+                    ltDate.setDate(today.getDate() - value);
+
+                    const gteDate: Date = new Date(today);
+                    gteDate.setDate(today.getDate() - (value + 1));
+
+                    return {
+                        name: `${ltDate.getDate()}-${ltDate.getMonth()}-${ltDate.getFullYear()}`,
+                        total: await this.prismaService[this.modelName].count({
+                            where: {
+                                createdAt: {
+                                    gte: gteDate.toISOString(),
+                                    lt: ltDate.toISOString(),
+                                },
+                            },
+                        }),
+                    };
+                })
+            );
+
+            this.loggerService.log(`Find Area Chart Total: ${JSON.stringify(data)}`);
+
+            return data;
+        } catch (error) {
+            this.loggerService.error(`Find Area Chart Total: ${error.message}`);
+            throw new InternalServerErrorException("Internal Server Error");
+        }
     }
 }
